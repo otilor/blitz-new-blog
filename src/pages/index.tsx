1595d21fe441a9e5e613239b555391082c84c36f
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2022-11-24 12:24:43 +0100

    clean up home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -62,66 +62,14 @@ const Home: BlitzPage = () => {
             <Image src={`${logo.src}`} alt="blitzjs" width="256px" height="118px" layout="fixed" />
           </div>
           <p>
-            <strong>Congrats!</strong> Your app is ready, including user sign-up and log-in.
+            <strong>Congrats!</strong> Welcome to Gabriel's Blitz.js app.
           </p>
           <div className="buttons" style={{ marginTop: "1rem", marginBottom: "1rem" }}>
             <Suspense fallback="Loading...">
               <UserInfo />
             </Suspense>
           </div>
-          <p>
-            <strong>
-              To add a new model to your app, <br />
-              run the following in your terminal:
-            </strong>
-          </p>
-          <pre>
-            <code>blitz generate all project name:string</code>
-          </pre>
-          <div style={{ marginBottom: "1rem" }}>(And select Yes to run prisma migrate)</div>
-          <div>
-            <p>
-              Then <strong>restart the server</strong>
-            </p>
-            <pre>
-              <code>Ctrl + c</code>
-            </pre>
-            <pre>
-              <code>blitz dev</code>
-            </pre>
-            <p>
-              and go to{" "}
-              <Link href="/projects">
-                <a>/projects</a>
-              </Link>
-            </p>
-          </div>
-          <div className="buttons" style={{ marginTop: "5rem" }}>
-            <a
-              className="button"
-              href="https://blitzjs.com/docs/getting-started?utm_source=blitz-new&utm_medium=app-template&utm_campaign=blitz-new"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Documentation
-            </a>
-            <a
-              className="button-outline"
-              href="https://github.com/blitz-js/blitz"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Github Repo
-            </a>
-            <a
-              className="button-outline"
-              href="https://discord.blitzjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Discord Community
-            </a>
-          </div>
+
         </main>
 
         <footer>
